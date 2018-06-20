9c8c2735eed8f998d1c1fa4645fc3132f0c00804
bwang0202@gmail.com <bojun@privatemachines.com>
2018-06-21 06:43:08 +0800

    type of teams bug

--- src/constructure/main/views.py
+++ src/constructure/main/views.py
@@ -16,14 +16,14 @@ DO_COMPETING = True
 education_enum = {"小学": 1, "初中": 2, "高中": 3, "本科": 4}
 certificate_enum = {"尚未认证": 0, "初级": 1, "中级": 2, "高级": 3, "技师": 4}
 projects_enum = {"单独住宅": 1, "住宅小区": 2, "公共建筑": 3, "办公楼": 4}
-teams_enum = {"世界企业": 1, "全国企业": 2, "地区领头企业": 3, "地区企业": 4}
+teams_enum = {"世界企业": 4, "全国企业": 3, "地区领头企业": 2, "地区企业": 1}
 
 
 def build_worker(body):
     worker = Worker(body['name'], body['age'], body['work_age'],
         education_enum.get(body['education'], 1), body['hometown'], body['jobs'], body['projects'],
         body['average_project_days'], projects_enum.get(body['type_of_projects'], 1),
-        body['num_of_teams'], teams_enum.get(body['type_of_teams'], 4))
+        body['num_of_teams'], teams_enum.get(body['type_of_teams'], 1))
     worker.specialities.append(body['speciality'])
     worker.certificates.append(certificate_enum.get(body['certificate'], 0))
     return worker
