c9e4df410be6ce7921a0578354e9ab60123483e6
bwang0202@gmail.com <bojun@privatemachines.com>
2018-07-02 12:03:45 +0800

    fixed api input enum

--- src/constructure/main/views.py
+++ src/constructure/main/views.py
@@ -16,9 +16,59 @@ DO_COMPETING = True
 
 education_enum = {"小学": 1, "初中": 2, "高中": 3, "本科": 4}
 certificate_enum = {"尚未认证": 0, "初级": 1, "中级": 2, "高级": 3, "技师": 4}
-projects_enum = {"单独住宅": 1, "住宅小区": 2, "公共建筑": 3, "办公楼": 4}
+projects_enum = {"单体住宅": 1, "住宅小区": 2, "公共建筑": 3, "办公楼": 4}
 teams_enum = {"世界企业": 4, "全国企业": 3, "地区领头企业": 2, "地区企业": 1}
 
+def to_age(age_input):
+    if age_input == "20 - 29":
+        return 20
+    if age_input == "30 - 39":
+        return 30
+    if age_input == "40 - 49":
+        return 40
+    return 50
+
+def to_jobs(jobs_input):
+    if jobs_input == "1 种":
+        return 1
+    if jobs_input == "2 种":
+        return 2
+    if jobs_input == "3 种":
+        return 3
+    return 4
+
+def to_work_age(work_age_input):
+    if work_age_input == "不到一年":
+        return 0
+    if work_age_input == "一到三年":
+        return 1
+    if work_age_input == "三到五年":
+        return 3
+    if work_age_input == "五年以上":
+        return 5
+    return 2
+
+def to_projects(projects_input):
+    if projects_input == "1 - 3个":
+        return 2
+    if projects_input == "4 - 6个":
+        return 5
+    if projects_input == "7 - 9 个":
+        return 8
+    if projects_input == "> 10个":
+        return 11
+    return 6
+
+def to_project_days(project_days):
+    if project_days == "1个月之内":
+        return 30
+    if project_days == "1个月 ~ 半年":
+        return 180
+    if project_days == "半年 ~ 一年":
+        return 360
+    if project_days == "一年以上":
+        return 500
+    return 200
 
 def test(request):
     print('this is a test log')
@@ -26,9 +76,10 @@ def test(request):
 
 
 def build_worker(body):
-    worker = Worker(body['name'], body['age'], body['work_age'],
-        education_enum.get(body['education'], 1), body['hometown'], body['jobs'], body['projects'],
-        body['average_project_days'], projects_enum.get(body['type_of_projects'], 1),
+    worker = Worker(body['name'], to_age(body['age']), to_work_age(body['work_age']),
+        education_enum.get(body['education'], 1), body['hometown'],
+        to_jobs(body['jobs']), to_projects(body['projects']),
+        to_project_days(body['average_project_days']), projects_enum.get(body['type_of_projects'], 1),
         body['num_of_teams'], teams_enum.get(body['type_of_teams'], 1))
     worker.specialities.append(body['speciality'])
     worker.certificates.append(certificate_enum.get(body['certificate'], 0))
